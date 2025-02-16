c6545570d69b7e129f290bfc6e5fe85de7ad07aa
prathamesh magar <prathameshma1@gmail.com>
2025-02-16 13:57:00 +0530

    intersection arr

--- EVALUATION/Arrays/UnionOfArr.cs
+++ EVALUATION/Arrays/UnionOfArr.cs
@@ -13,8 +13,31 @@ class UnionOfArr
         int m = arr2.Length;
 
         int[] Union = UnionArr(arr1, arr2, n, m);
+        int[] intrSetArr = InterSectorArr(arr1, arr2, n, m);
         Console.WriteLine("Union Arr of arr1 and arr2 is: " + string.Join(", ", Union));
+        Console.WriteLine("Intersection Arr of arr1 and arr2 is: " + string.Join(", ", intrSetArr));
+
+
+
     }
+     
+      public static int[] InterSectorArr(int[] arr1, int[] arr2, int n, int m){
+
+        int[] ansArr = {};
+
+
+        for(int i = 0; i < n; i++){
+            if(arr2.Contains(arr1[i])){
+                ansArr.Add(arr1[i]);
+            }
+        }
+
+        return ansArr;
+
+      }
+   
+
+
 
     public static int[] UnionArr(int[] arr1, int[] arr2, int n, int m)
     {
