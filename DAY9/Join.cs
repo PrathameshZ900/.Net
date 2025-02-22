3d432244548bb70310e06418149b2809bc19b07c
prathamesh magar <prathameshma1@gmail.com>
2025-02-22 23:04:29 +0530

    ok

---
+++ DAY9/Join.cs
@@ -0,0 +1,21 @@
+// Understanding JOINS (Theoretical)
+// Explain the difference between INNER JOIN, LEFT JOIN, RIGHT JOIN, and FULL OUTER JOIN with real-world examples.
+
+// ANS: 
+
+// INNER JOIN →  take only commen elements from both tables.
+// LEFT JOIN →  take only from left tables, priority for left table.
+// RIGHT JOIN → take only from right tables, priority for right table.
+// FULL OUTER JOIN → take from both tables, priority for both tables.
+
+// Real-World Example (Customers & Orders)
+// INNER JOIN → Customers who placed orders
+// LEFT JOIN → All customers, even if no orders
+// RIGHT JOIN → All orders, even if customer deleted
+// FULL OUTER JOIN → All customers & orders, even if unrelated
+
+
+
+
+
+
